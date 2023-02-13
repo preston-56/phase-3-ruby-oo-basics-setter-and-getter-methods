30cc2068df90949e88fd17c051f6c00ae540056a
Preston <prestonosoro56@gmail.com>
2023-02-13 10:33:46 +0300

    feat:add setter and getter methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    #setter name method
+    def name=(name)
+        @name = name
+    end
 
+    #getter name method
+    def name
+        @name
+    end
+
+    #setter breed method
+    def breed=(breed)
+        @breed = breed
+    end
+    #getter breed method
+    def breed
+        @breed
+    end
 end
