ba7ceb1b3560440d258ff948440a40e9696789fb
Preston <prestonosoro56@gmail.com>
2023-02-13 10:33:04 +0300

    commit tree f985184bc0160f49a7d6e2c2b350df368585dcb5

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    #setter name method
+    def name=(name)
+        @name=name
+    end
+    #getter name method
+    def name
+        @name
+    end
 
+    #setter job method
+    def job=(job)
+        @job=job
+    end
+
+    #getter job method
+    def job
+        @job
+    end
 end
